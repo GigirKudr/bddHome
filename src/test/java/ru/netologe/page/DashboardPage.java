19c5aaae60a7c82d9c6a2c7921db7ce69ea63960
Anton Kudryashov <driver_90@list.ru>
2024-12-23 22:06:43 +0700

    Redakt with remark

--- src/test/java/ru/netologe/page/DashboardPage.java
+++ src/test/java/ru/netologe/page/DashboardPage.java
@@ -22,11 +22,6 @@ public class DashboardPage {
         return extractBalance(text);
     }
 
-    public int getCardBalance(int index) {
-        var text = cards.get(index).getText();
-        return extractBalance(text);
-    }
-
     public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
         cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestId())).$("button").click();
         return new TransferPage();
